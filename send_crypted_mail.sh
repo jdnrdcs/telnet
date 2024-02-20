a00a277df23bf8d216b10cf6cf73d5dff1a8d52b
jdnrdcs <86047343+jdnrdcs@users.noreply.github.com>
2024-02-20 20:34:23 +0200

    Create send_crypted_mail.sh

---
+++ send_crypted_mail.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "helo rtir"
+echo "mail from: nrdcsdev@gmail.com"
+echo "rcpt to: nrdcsdev@gmail.com"
+echo "data"
+echo "-----BEGIN PGP MESSAGE-----
+hQIMAwD2oJJdByyDAQ//ZnErCDa1569lQmvsOWV6lvawkI5K4kGW/JsuYv9opGOP
+H7gzpomYo19XA9GvcXwLw+n+gEKOeyqPAAHNNUjxmbKbBNiAmmVXCT5CBl/FqKfn
+TNh05XN66a0kvjeUmWnzC2fAinHsfrtS0mqHllyBBECF+BcTvACVVSEnv/kLwTNS
+2GeE2JrXrj7P+qnqFrxXCrfzV0tqh3TqiGGtv/4/32bA9RDNwc7hx3GHnkM/fCOo
+gue1eCWuXli8m9JGcEd9gPXsY82hXBN21YWzPcUZf0QNge6QccVW8oKE713agNU2
+XXnRj4+8vo2TXweoK1JCwovYAM14MV4aZWLVxfLtky+KsrSn4mSESVKcS5BR/7qu
+dN3t0wXmB1hB4pysyKo/7L3XUREdjZXVaYt8LuN62oxv+lRG0jv8bkDXGdPyRIx+
+R2HEwR/vvwCYFMW7qyDYRGxcvvmmOZqasmm91E5e9l7NjtmvrxC/1veC5IibIs/C
+ZhqAOs1nXcrmL1tjyQETYrqLNKPDMF/xneY/SSuqXbWnJLE3m5PvoDzr6jNPAxWC
+ghPeCsCqr4z6sz8uXQdVRm/dVv9YLIu/tdYqOOwjgsxcb8vx80OsJsi1r+Kbx2lj
+6pjAOVvlBBNCOXWHwFkyhQG6C7mRC/NqtIvJI0MIN5CpCotLkBcoSUI0lOeK3vDS
+wJ4BMevTbqvqY3AUkmQuYbaqeI7kJVMzCbKTagtv3Kh9RYX2RB3ZWv94DJ8FJQD8
+C+TLgNF7cYHV8hguhCeNYBXYOUlIlQ6CN4KJOxT/04B4ZklsIBvj5+j6c4xQTV30
+xhnJPOen15ZvRKRzvrkuQN3fW6Sg7NUKW77q4K5C/k/OaRexOsJ40x+OGYKyMmSu
+kO89Tc2eJTP7s8n9Mqla6ymZDGoRcOXJPt+iSpi6/rUU5Ltx0HCvV+G5ESwcvlsk
+en+QJTyXO9k8llS9MrQ0qO90PlxDgEkc0CmvM0X+pL9yhbZ+Ods0YMWrGAmdjwh5
+nrdoeGDalBbrjYwpbn/hVRWRXc7PeuufIUYaRCqr2JeuGHUdcEDRQ+jWFBaLwSGf
+3ZDZ+u7+y7E3XvQWaBJVKJoQzJAg6CbkpWKIeSlUWKuYta6NSQOAuHqGtP9shiCS
+X/Hw/zFRxUVavx2qLH2dSA==
+=3NAy
+-----END PGP MESSAGE-----"
+echo "."
+echo "sleep 2"
+echo "quit"
